f3eda7fc762abd49b02e3fe2a384598a48e6015c
YorkYoung <osyxz2011@gmail.com>
2014-12-27 16:59:33 +0800

    Update bilibili.tv-to-bilibili.com.js
    It can be used now, thanks to https://nightlyfantasy.github.io/Bili_Fix_Player

--- bilibili.tv-to-bilibili.com.js
+++ bilibili.tv-to-bilibili.com.js
@@ -1,16 +1,19 @@
 // ==UserScript==
 // @name         bilibili.tv to bilibili.com
-// @namespace    https://github.com/YorkYoung/bilibili.tv-to-bilibili.com/blob/master/bilibili.tv-to-bilibili.com.js
-// @version      1.0
+// @namespace    https://github.com/YorkYoung/bilibili.tv-to-bilibili.com/
+// @downloadURL  https://github.com/YorkYoung/bilibili.tv-to-bilibili.com/blob/master/bilibili.tv-to-bilibili.com.js
+// @version      1.1
+// @encoding     utf-8
 // @description  UserScript to change bilibili.tv to bilibili.com
 // @author       YorkYoung
-// @match        http://www.bilibili.tv/*
+// @license      GPL version 3
+// @include      /^.*\.bilibili\.(tv|com|cn)\/(video\/|search)?.*$/
 // @grant        none
 // ==/UserScript==
 
 document.body.addEventListener('click', function(e){
     var targ = e.target || e.srcElement;
-    if ( targ && targ.href && targ.href.match('http:\/\/www.bilibili.tv') ) {
-        targ.href = targ.href.replace('://www.bilibili.tv', '://www.bilibili.com');
+    if ( targ && targ.href && targ.href.match('bilibili.tv') ) {
+        targ.href = targ.href.replace('bilibili.tv', 'bilibili.com');
     }
 });
